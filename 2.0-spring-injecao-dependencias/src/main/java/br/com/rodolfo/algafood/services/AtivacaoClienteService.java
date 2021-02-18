feae00d834eecb5ff42effa5e5120ba5c5ed4206
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-02-18 08:31:46 -0300

    2.0-Ciclo de vida dos beans utilizando classe de configuração com anaotações @Configuration e @Beans

--- 2.0-spring-injecao-dependencias/src/main/java/br/com/rodolfo/algafood/services/AtivacaoClienteService.java
+++ 2.0-spring-injecao-dependencias/src/main/java/br/com/rodolfo/algafood/services/AtivacaoClienteService.java
@@ -1,19 +1,25 @@
 package br.com.rodolfo.algafood.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import br.com.rodolfo.algafood.models.Cliente;
 import br.com.rodolfo.algafood.notificador.Notificador;
 import br.com.rodolfo.algafood.notificador.TipoNotificador;
 import br.com.rodolfo.algafood.notificador.enums.NivelUrgencia;
 
-@Component
 public class AtivacaoClienteService {
     
     @TipoNotificador(NivelUrgencia.NAO_URGENTE)
     @Autowired
     private Notificador notificador;
+
+    public void init() {
+        System.out.println("INIT");
+    }
+
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
