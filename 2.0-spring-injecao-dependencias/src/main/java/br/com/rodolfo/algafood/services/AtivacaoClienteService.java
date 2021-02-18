33d647fc6e499bbc95a8ed50e7f8aea28e59ce53
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-02-18 08:33:10 -0300

    2.0-Ciclo de vida dos beans utilizando as anotações @PostConstruct e @PreDestroy

--- 2.0-spring-injecao-dependencias/src/main/java/br/com/rodolfo/algafood/services/AtivacaoClienteService.java
+++ 2.0-spring-injecao-dependencias/src/main/java/br/com/rodolfo/algafood/services/AtivacaoClienteService.java
@@ -1,22 +1,29 @@
 package br.com.rodolfo.algafood.services;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import br.com.rodolfo.algafood.models.Cliente;
 import br.com.rodolfo.algafood.notificador.Notificador;
 import br.com.rodolfo.algafood.notificador.TipoNotificador;
 import br.com.rodolfo.algafood.notificador.enums.NivelUrgencia;
 
+@Component
 public class AtivacaoClienteService {
     
     @TipoNotificador(NivelUrgencia.NAO_URGENTE)
     @Autowired
     private Notificador notificador;
 
+    @PostConstruct
     public void init() {
         System.out.println("INIT");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("DESTROY");
     }
