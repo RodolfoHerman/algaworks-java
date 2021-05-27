2c89e6e27ef779c68f753958680688a92e1ed830
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-05-26 21:44:35 -0300

    3.1-Mapeando e criando as tabelas do banco de dados a partir das entidades

---
+++ 3.0-spring-introducao-jpa-hibernate/src/main/java/br/com/rodolfo/algafood/domain/models/Cozinha.java
@@ -0,0 +1,24 @@
+package br.com.rodolfo.algafood.domain.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String nome;
+}
---
+++ 3.0-spring-introducao-jpa-hibernate/src/main/java/br/com/rodolfo/algafood/domain/models/Restaurante.java
@@ -0,0 +1,28 @@
+package br.com.rodolfo.algafood.domain.models;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+}
