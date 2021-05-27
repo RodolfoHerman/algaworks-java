272d77d4d4e8751a2ecf9b2caa37625f1db74154
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-05-27 07:56:12 -0300

    3.4-Adicionando objetos no banco de dados (EntityManager)

--- 3.0-spring-introducao-jpa-hibernate/src/main/java/br/com/rodolfo/algafood/jpa/CadastroCozinha.java
+++ 3.0-spring-introducao-jpa-hibernate/src/main/java/br/com/rodolfo/algafood/jpa/CadastroCozinha.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.rodolfo.algafood.domain.models.Cozinha;
 
@@ -18,5 +19,10 @@ public class CadastroCozinha {
     public List<Cozinha> listar() {
         return manager.createQuery("from Cozinha", Cozinha.class)
             .getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        return manager.merge(cozinha);
     }
 }
