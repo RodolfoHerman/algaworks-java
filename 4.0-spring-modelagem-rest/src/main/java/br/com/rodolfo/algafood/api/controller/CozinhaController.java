aa3efd49833d5f2bdf3461f12c39e4f9f397c55f
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-06-11 07:50:08 -0300

    4.6-Modelando e implementando a inclusão de recursos com POST (Cozinha)

--- 4.0-spring-modelagem-rest/src/main/java/br/com/rodolfo/algafood/api/controller/CozinhaController.java
+++ 4.0-spring-modelagem-rest/src/main/java/br/com/rodolfo/algafood/api/controller/CozinhaController.java
@@ -9,7 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rodolfo.algafood.api.model.CozinhasXmlWrapper;
@@ -42,5 +45,11 @@ public class CozinhaController {
         }
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    Cozinha salvar(@RequestBody Cozinha cozinha) {
+        return cozinhaRepository.salvar(cozinha);
     }
 }
