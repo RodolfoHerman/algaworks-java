e2ecb158a3ff74e5cc3a722edf3893885b79666e
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-06-11 07:32:52 -0300

    4.5-Corrigindo o Status HTTP para resource inexistente (ResponseEntity)

--- 4.0-spring-modelagem-rest/src/main/java/br/com/rodolfo/algafood/api/controller/CozinhaController.java
+++ 4.0-spring-modelagem-rest/src/main/java/br/com/rodolfo/algafood/api/controller/CozinhaController.java
@@ -1,9 +1,12 @@
 package br.com.rodolfo.algafood.api.controller;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +34,13 @@ public class CozinhaController {
     }
 
     @GetMapping("/{cozinha-id}")
-    Cozinha buscar(@PathVariable("cozinha-id") Long id) {
-        return cozinhaRepository.buscar(id);
+    ResponseEntity<Cozinha> buscar(@PathVariable("cozinha-id") Long id) {
+        Cozinha cozinha = cozinhaRepository.buscar(id);
+
+        if(Objects.nonNull(cozinha)) {
+            return ResponseEntity.ok(cozinha);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 }
