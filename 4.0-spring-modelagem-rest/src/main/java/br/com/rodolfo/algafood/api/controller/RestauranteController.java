5dba9aa3f08f4576665ccbcfc873e18edafd8e6e
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2021-06-29 19:52:17 -0300

    4.16-Modelando e implementando a atualização parcial de recursos com PATCH (Restaurante)

--- 4.0-spring-modelagem-rest/src/main/java/br/com/rodolfo/algafood/api/controller/RestauranteController.java
+++ 4.0-spring-modelagem-rest/src/main/java/br/com/rodolfo/algafood/api/controller/RestauranteController.java
@@ -1,13 +1,19 @@
 package br.com.rodolfo.algafood.api.controller;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -75,5 +81,35 @@ public class RestauranteController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PatchMapping("/{restaurante-id}")
+    public ResponseEntity<?> autualizarParcial(
+        @PathVariable("restaurante-id") Long id,
+        @RequestBody Map<String, Object> values
+    ) {
+        Restaurante restaurante = restauranteRepository.buscar(id);
+
+        if(Objects.nonNull(restaurante)) {
+            merge(values, restaurante);
+
+            return atualizar(id, restaurante);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    private void merge(Map<String, Object> values, Restaurante restaurante) {
+        ObjectMapper mapper = new ObjectMapper();
+        Restaurante restauranteOrigem = mapper.convertValue(values, Restaurante.class);
+
+        values.forEach((key, value) -> {
+            Field field = ReflectionUtils.findField(Restaurante.class, key);
+            field.setAccessible(Boolean.TRUE);
+
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+            ReflectionUtils.setField(field, restaurante, novoValor);
+        });
     }
 }
