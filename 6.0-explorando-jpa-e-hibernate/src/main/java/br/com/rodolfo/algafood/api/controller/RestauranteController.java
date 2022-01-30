bf934a6a5f15bb38b4ec3cf1b8878defc20f8510
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-01-30 09:23:57 -0300

    6.5-Realizando mapeamentos das entidades Grupo, Produto, Restaurante e Usuario

--- 6.0-explorando-jpa-e-hibernate/src/main/java/br/com/rodolfo/algafood/api/controller/RestauranteController.java
+++ 6.0-explorando-jpa-e-hibernate/src/main/java/br/com/rodolfo/algafood/api/controller/RestauranteController.java
@@ -72,7 +72,7 @@ public class RestauranteController {
         if(restauranteSalvo.isPresent()) {
             try {
                 BeanUtils.copyProperties(restaurante, restauranteSalvo.get(), 
-                    "id", "formasPagamento", "endereco", "dataCadastro");
+                    "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
                 Restaurante restauranteNovo = cadastroRestauranteService.salvar(restauranteSalvo.get());
 
                 return ResponseEntity.ok(restauranteNovo);
