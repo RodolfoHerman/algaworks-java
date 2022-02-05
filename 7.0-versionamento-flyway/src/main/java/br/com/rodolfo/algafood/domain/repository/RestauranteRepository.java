ef55c9bf6ff7b90a498bec2ad05915fe4b3778d6
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-02-05 09:38:12 -0300

    7.3-Adicionando dados de testes com callback do Flyway

--- 7.0-versionamento-flyway/src/main/java/br/com/rodolfo/algafood/domain/repository/RestauranteRepository.java
+++ 7.0-versionamento-flyway/src/main/java/br/com/rodolfo/algafood/domain/repository/RestauranteRepository.java
@@ -12,7 +12,7 @@ import br.com.rodolfo.algafood.domain.models.Restaurante;
 
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 
-    @Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+    @Query("from Restaurante r join fetch r.cozinha")
     List<Restaurante> findAll();
 
     List<Restaurante> findTodosByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
