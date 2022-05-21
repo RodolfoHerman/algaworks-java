0b2862edd52e4152e26a6ef50d69cc0cc018e732
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-05-21 19:01:09 -0300

    10.5-Limpando e populando o banco de dados de teste

--- 10.0-testes-de-integracao/src/test/java/br/com/rodolfo/algafood/CadastroCozinhaIT.java
+++ 10.0-testes-de-integracao/src/test/java/br/com/rodolfo/algafood/CadastroCozinhaIT.java
@@ -1,6 +1,5 @@
-package br.com.rodolfo.algafood.domain.service;
+package br.com.rodolfo.algafood;
 
-import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,20 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import br.com.rodolfo.algafood.domain.models.Cozinha;
+import br.com.rodolfo.algafood.domain.repository.CozinhaRepository;
+import br.com.rodolfo.algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private Flyway flyway;
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
 
     @BeforeEach
     void setUp() {
@@ -30,7 +37,8 @@ public class CadastroCozinhaIT {
         RestAssured.basePath = "/cozinhas";
         RestAssured.port = port;
 
-        flyway.migrate();
+        databaseCleaner.clearTables();
+        prepararDadosTeste();
     }
 
     @Test
@@ -44,13 +52,35 @@ public class CadastroCozinhaIT {
     }
 
     @Test
-    void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+    void deveConter2Cozinhas_QuandoConsultarCozinhas() {
         RestAssured.given()
             .accept(ContentType.JSON)
         .when()
             .get()
         .then()
-            .body("", Matchers.hasSize(4))
+            .body("", Matchers.hasSize(2))
             .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+    }
+
+    @Test
+    void deveRetornarHttpStatus201_QuandoCadastrarCozinha() {
+        RestAssured.given()
+            .body("{ \"nome\": \"Chinesa\" }")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
+    }
+
+    private void prepararDadosTeste() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Indiana");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha2);
     }
 }
