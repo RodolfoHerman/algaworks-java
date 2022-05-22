4e2bbce9b4fda1503db1b39a74bddcacc67db3d9
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-05-22 16:20:08 -0300

    11.2-Refinando o payload de cadastro com @JsonIgnoreProperties

--- 11.0-boas-praticas-e-tecnicas-para-api/src/main/java/br/com/rodolfo/algafood/domain/models/Restaurante.java
+++ 11.0-boas-praticas-e-tecnicas-para-api/src/main/java/br/com/rodolfo/algafood/domain/models/Restaurante.java
@@ -24,6 +24,7 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -54,6 +55,7 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
+    @JsonIgnoreProperties(value = "nome", allowGetters = true)
     @Valid
     @NotNull
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
