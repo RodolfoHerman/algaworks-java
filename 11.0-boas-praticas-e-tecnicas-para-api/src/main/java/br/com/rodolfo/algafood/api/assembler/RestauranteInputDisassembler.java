48437290f9b95536f45c84e01cb2059b1d202a37
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-06-14 19:14:27 -0300

    11.8-Adicionando e usando o ModelMapper (evitando boilerplate)

--- 11.0-boas-praticas-e-tecnicas-para-api/src/main/java/br/com/rodolfo/algafood/api/assembler/RestauranteInputDisassembler.java
+++ 11.0-boas-praticas-e-tecnicas-para-api/src/main/java/br/com/rodolfo/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -1,24 +1,19 @@
 package br.com.rodolfo.algafood.api.assembler;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.rodolfo.algafood.api.model.input.RestauranteInput;
-import br.com.rodolfo.algafood.domain.models.Cozinha;
 import br.com.rodolfo.algafood.domain.models.Restaurante;
 
 @Component
 public class RestauranteInputDisassembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
--- 11.0-boas-praticas-e-tecnicas-para-api/src/main/java/br/com/rodolfo/algafood/api/assembler/RestauranteModelAssembler.java
+++ 11.0-boas-praticas-e-tecnicas-para-api/src/main/java/br/com/rodolfo/algafood/api/assembler/RestauranteModelAssembler.java
@@ -3,27 +3,21 @@ package br.com.rodolfo.algafood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import br.com.rodolfo.algafood.api.model.CozinhaModel;
 import br.com.rodolfo.algafood.api.model.RestauranteModel;
 import br.com.rodolfo.algafood.domain.models.Restaurante;
 
 @Component
 public class RestauranteModelAssembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public RestauranteModel toModel(Restaurante restaurante) {
-        CozinhaModel cozinhaModel = new CozinhaModel();
-        cozinhaModel.setId(restaurante.getCozinha().getId());
-        cozinhaModel.setNome(restaurante.getCozinha().getNome());
-
-        RestauranteModel restauranetModel = new RestauranteModel();
-        restauranetModel.setId(restaurante.getId());
-        restauranetModel.setNome(restaurante.getNome());
-        restauranetModel.setTaxaFrete(restaurante.getTaxaFrete()); 
-        restauranetModel.setCozinha(cozinhaModel);
-
-        return restauranetModel;
+        return modelMapper.map(restaurante, RestauranteModel.class);
     }
 
     public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes) {
