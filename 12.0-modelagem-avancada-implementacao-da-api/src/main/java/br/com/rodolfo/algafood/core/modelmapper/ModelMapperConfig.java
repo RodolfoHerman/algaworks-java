460cc0dc693a2a255f2e67faf74f06a7c88d6b50
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-06-21 10:15:50 -0300

    12.3-Adicionando endereço no modelo da representação do recurso de restaurante

--- 12.0-modelagem-avancada-implementacao-da-api/src/main/java/br/com/rodolfo/algafood/core/modelmapper/ModelMapperConfig.java
+++ 12.0-modelagem-avancada-implementacao-da-api/src/main/java/br/com/rodolfo/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,21 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.rodolfo.algafood.api.model.EnderecoModel;
+import br.com.rodolfo.algafood.domain.models.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
     @Bean
     ModelMapper modelMapper() {
-        return new ModelMapper();
+        var modelMap = new ModelMapper();
+
+        var enderecoToEnderecoModelTypeMap = modelMap.createTypeMap(Endereco.class, EnderecoModel.class);
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+            endereco -> endereco.getCidade().getEstado().getNome(),
+            (enderecoModel, value) -> enderecoModel.getCidade().setEstado(value));
+
+        return modelMap;
     }
 }
