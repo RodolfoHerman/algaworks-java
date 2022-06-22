b54872d604cf58808faf8f2447d5804398e02225
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-06-22 14:57:47 -0300

    12.7-Implementando regra de negócio para evitar usuários com e-mails duplicados

--- 12.0-modelagem-avancada-implementacao-da-api/src/main/java/br/com/rodolfo/algafood/domain/repository/UsuarioRepository.java
+++ 12.0-modelagem-avancada-implementacao-da-api/src/main/java/br/com/rodolfo/algafood/domain/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package br.com.rodolfo.algafood.domain.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 
 import br.com.rodolfo.algafood.domain.models.Usuario;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> { }
+public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+}
