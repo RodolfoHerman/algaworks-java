b684479d991fc50ba7bed65c2674449f1c6ad1e2
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-07-24 16:10:49 -0300

    14.2-Validando o tamanho máximo do arquivo com Bean Validation

--- 14.0-upload-download-de-arquivos/src/main/java/br/com/rodolfo/algafood/api/model/input/FotoProdutoInput.java
+++ 14.0-upload-download-de-arquivos/src/main/java/br/com/rodolfo/algafood/api/model/input/FotoProdutoInput.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import br.com.rodolfo.algafood.core.validation.FileSize;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import lombok.Setter;
 public class FotoProdutoInput {
 
     @NotNull
+    @FileSize(max = "500KB")
     private MultipartFile arquivo;
 
     @NotBlank
