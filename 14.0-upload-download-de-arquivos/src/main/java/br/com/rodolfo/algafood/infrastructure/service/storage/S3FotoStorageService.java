b58afa946f8debea6604e793934b7e70a48457ba
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-08-18 20:00:59 -0300

    14.14-Definindo bean do cliente Amazon S3 e as credenciais

---
+++ 14.0-upload-download-de-arquivos/src/main/java/br/com/rodolfo/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -0,0 +1,29 @@
+package br.com.rodolfo.algafood.infrastructure.service.storage;
+
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.s3.AmazonS3;
+
+import br.com.rodolfo.algafood.domain.service.FotoStorageService;
+
+@Service
+public class S3FotoStorageService implements FotoStorageService {
+
+    @Autowired
+    private AmazonS3 amazonS3;
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        return null;
+    }
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) { }
+
+    @Override
+    public void remover(String nomeFoto) { }
+
+}
